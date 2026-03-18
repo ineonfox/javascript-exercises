7411246f1599501c3552ab562ff5d0281dee33a8
ineonfox <61496409+ineonfox@users.noreply.github.com>
2026-03-19 00:10:34 +0100

    Finish foundations/exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ((!Number.isInteger(num1) || !Number.isInteger(num2)) || 
+        (num1 < 0 || num2 < 0)) {
+        return 'ERROR';
+    }
 
+    let start = Math.min(num1, num2);
+    let end = Math.max(num1, num2);
+
+    let sumArray = [];
+    for (let i = start; i <= end; i++) {
+        sumArray.push(i);
+    }
+    return sumArray.reduce((acc, cur) => acc + cur);
 };
 
 // Do not edit below this line
