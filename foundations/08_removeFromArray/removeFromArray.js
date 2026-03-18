40bf8f0793cea3de94d5c030d63f9947ec3c5048
ineonfox <61496409+ineonfox@users.noreply.github.com>
2026-03-18 23:05:28 +0100

    Finish foundations/exercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const valuesToRemove = Array.prototype.slice.call(arguments, 1);
+    let filteredArray = arr.slice();
+    for (value of valuesToRemove) {
+        filteredArray = filteredArray.filter(v => v !== value);
+    }
+    return filteredArray;
 };
 
 // Do not edit below this line
