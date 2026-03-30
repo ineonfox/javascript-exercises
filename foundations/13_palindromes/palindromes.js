d79e50587d331af92063cac9184894b45f72948d
ineonfox <61496409+ineonfox@users.noreply.github.com>
2026-03-30 23:57:45 +0200

    Finish exercise foundations/13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const fixedString = str
+        .replace(/[ .,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+        .toLowerCase();
+    const reversedString = fixedString
+        .split('')
+        .reverse()
+        .join('');
+    return fixedString === reversedString;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
