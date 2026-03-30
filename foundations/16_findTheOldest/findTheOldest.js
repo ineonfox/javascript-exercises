74a88df279f76c4d209d5e7396ee906103419eb4
ineonfox <61496409+ineonfox@users.noreply.github.com>
2026-03-31 00:17:08 +0200

    Finish exercise foundations/16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        if (getAge(oldest) > getAge(currentPerson)) {
+            return oldest;
+        }
+        return currentPerson;
+    }, people[0]);
 };
 
+function getAge(person) {
+    let age;
+    if ('yearOfDeath' in person) {
+        age = person.yearOfDeath - person.yearOfBirth;
+    }
+    else {
+        let currentYear = (new Date()).getFullYear();
+        age = currentYear - person.yearOfBirth;
+    }
+    return age;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
