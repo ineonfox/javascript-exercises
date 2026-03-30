f7dd4e2514c56f53d25ababa1b188b5263389702
ineonfox <61496409+ineonfox@users.noreply.github.com>
2026-03-31 00:02:43 +0200

    Finish exercise foundations/14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let prev = 1;
+    let current = 1;
+    let number = Number(num);
+    if (number === 0) {
+        return 0;
+    }
+    if (number < 0) {
+        return 'OOPS';
+    }
+    for (let i = 3; i <= number; i++) {
+        [prev, current] = [current, prev + current];
+    }
+    return current;
 };
 
 // Do not edit below this line
